bdc1b62eacc26b14eaf7e884ad5378de1d9a3db7
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-01-19 14:43:17 +0530

    loaded router, set up view engine and set up static files

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const port = 8000;
 const app = express();
 const db = require('./config/mongoose');
 
+// Add express router
 
+app.use('/', require('./routes'));
+
+// setting up ejs
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// setting up static files
+
+app.use(express.static('./assets'));
 
 
 
