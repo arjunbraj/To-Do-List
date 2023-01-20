29fb0b6bc1713b7c36377444caeb9c6dd6deaf05
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-01-20 17:26:33 +0530

    debugged database entry issue

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', true);
 
 // connecting to database
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/to_di_list_dev');
+    await mongoose.connect('mongodb://127.0.0.1:27017/to_do_list_dev');
 }
 
 main().catch(err => console.log(err));
